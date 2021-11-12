4dc4c9791e8a074f7afaa11e136603600d23b1e6
Brayden Karnes <90624973+Senrak@users.noreply.github.com>
2021-11-12 16:31:25 -0500

    Added beta features.  Still in the writing stage.

--- VirtTheBarTender.py
+++ VirtTheBarTender.py
@@ -44,19 +44,55 @@ def main():
         random_ten()
     elif choice == str(2):
         drink_search()
+    elif choice == str(3):
+        popular()
+    elif choice == str(4):
+        non_alcoholic()
+    elif choice == str(5):
+        testing_1()
+
+def testing_1():
+    url_appended = url + url_random
+    
+    response = requests.request("GET", url_appended, headers=headers).json()
+
+    print(response)
+
+
+def non_alcoholic():
+    url_appended = url + url_filter
+    querystring = {"a":"Non alcoholic"}
+
+    response = requests.request("GET", url_appended, headers=headers, params=querystring).json()
+
+    drink_num = len(response['drinks'])
+        
+    print('')
+    for x in range(drink_num):
+        print(f'{response["drinks"][x]["strDrink"] : <20}')
+
+def popular():
+    url_appended = url + url_popular
+
+    response = requests.request("GET", url_appended, headers=headers).json()
+    
+    print('')
+    print(f'{"Top 10 Drinks:" : <20}        {"Ingredients:" : <25}')
+
+    for x in range(10):
+        print(f'{response["drinks"][x]["strDrink"] : <20}  {response["drinks"][x]["strIngredient1"] : <25} {"& more" : <28}')
 
 def testing_menu():
     print()
     print('1. random_ten')
     print('2. drink_search')
+    print('3. popular')
+    print('4. non_alcoholic')
     choice = input('Enter choice: ')
     
     return choice
 
 def random_ten():
-
-    #VARIABLES
-    ingredients = []
     url_appended = url + url_multirandom
 
     response = requests.request("GET", url_appended, headers=headers).json()
@@ -69,9 +105,7 @@ def random_ten():
             var = 'strIngredient' + str(y+1)
             if (response['drinks'][x][var]) == None:
                 break
-            ingredients.append(response['drinks'][x][var])
             print(response['drinks'][x][var])
-        #print(*ingredients, sep = ", ")
         print('Instructions: ',response['drinks'][x]['strInstructions'])
 
 def drink_search():
